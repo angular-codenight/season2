1fdfb336f509c8ea1c37fbf7bbc7cf1ce1abb91d
sajjadmaneshi <maneshisajjad@gmail.com>
2024-01-19 15:56:50 +0330

    type of selectors

--- src/app/todo-list/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list/todo-list-item/todo-list-item.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list-item',
+  // selector: 'app-todo-list-item',
+  // selector: '[app-todo-list-item]', //attribute selector
+  // selector: '.app-todo-list-item', // class selector
   templateUrl: './todo-list-item.component.html',
   styleUrls: ['./todo-list-item.component.scss']
 })
