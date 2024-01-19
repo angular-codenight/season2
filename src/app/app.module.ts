2bd71ad3d79121128db187ff9df7b2c3f972618b
sajjadmaneshi <maneshisajjad@gmail.com>
2024-01-19 15:42:45 +0330

    create component with angular/cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {TodoListComponent} from "./todo-list/todo-list.component";
+import { TodoListItemComponent } from './todo-list/todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoListItemComponent
   ],
   imports: [
     BrowserModule
